f2c9afb087d83dea87dd8a20e3936332e8acc2ff
CodeProject <nguyenduy27092002@gmail.com>
2022-09-08 21:35:07 +0700

    comment block code in JpaAuditingConfig

--- language-courses/src/main/java/com/javaproject/admin/config/JpaAuditingConfig.java
+++ language-courses/src/main/java/com/javaproject/admin/config/JpaAuditingConfig.java
@@ -6,26 +6,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
 
-@Configuration
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+//@Configuration
+//@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfig {
-	@Bean
-	AuditorAware<String> auditorProvider() {
-		return new AuditorAwareImpl();
-	}
-
-	public static class AuditorAwareImpl implements AuditorAware<String> {
-
-		@Override
-		public Optional<String> getCurrentAuditor() {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			if (authentication == null) {
-				return null;
-			}
-			return Optional.of(authentication.getName());
-		}
-	}
+//	@Bean
+//	AuditorAware<String> auditorProvider() {
+//		return new AuditorAwareImpl();
+//	}
+//
+//	public static class AuditorAwareImpl implements AuditorAware<String> {
+//
+//		@Override
+//		public Optional<String> getCurrentAuditor() {
+//			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//			if (authentication == null) {
+//				return null;
+//			}
+//			return Optional.of(authentication.getName());
+//		}
+//	}
 }
