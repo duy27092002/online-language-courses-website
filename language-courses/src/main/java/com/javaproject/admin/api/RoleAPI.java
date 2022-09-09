644aeab571d4c45f77b581656eb8079d72d8f368
CodeProject <nguyenduy27092002@gmail.com>
2022-09-09 09:31:09 +0700

    handle exception when get role list

--- language-courses/src/main/java/com/javaproject/admin/api/RoleAPI.java
+++ language-courses/src/main/java/com/javaproject/admin/api/RoleAPI.java
@@ -1,5 +1,7 @@
 package com.javaproject.admin.api;
 
+import javax.validation.constraints.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaproject.admin.dto.RoleDTO;
+import com.javaproject.admin.exception.BadRequestException;
 import com.javaproject.admin.service.IRoleService;
 import com.javaproject.admin.util.SortUtil;
 
@@ -24,22 +27,27 @@ public class RoleAPI {
 	
 	@GetMapping
 	public ResponseEntity<RoleDTO> showRoleListPage(
-			@RequestParam(name = "page", required = false, defaultValue = "1") int page,
+			@Pattern(regexp = "^.+$") @RequestParam(value = "page", required = false, defaultValue = "1") String page,
 			@RequestParam(name = "order-by", required = false, defaultValue = "name") String orderBy,
 			@RequestParam(name = "order-type", required = false, defaultValue = "asc") String orderType,
 			@RequestParam(name = "keyword", required = false) String keyword) {
 		RoleDTO roleDTO = new RoleDTO();
-		
-		if (keyword != null) {
-			
-		} else {
+		try {
+			int getPage = Integer.parseInt(page);
 			int pageSize = 2;
-			roleDTO.setCurrentPage(page);
+			roleDTO.setCurrentPage(getPage);
 			roleDTO.setPageTotal(roleService.getTotalPage(pageSize));
-			Pageable pageable = PageRequest.of(page - 1, pageSize, sortUtil.handleSord(orderBy, orderType));
-			roleDTO.setResultList(roleService.getListByPage(pageable));
+			Pageable pageable = PageRequest.of(getPage - 1, pageSize, sortUtil.handleSord(orderBy, orderType));
+			
+			if (keyword != null) {
+				
+			} else {
+				roleDTO.setResultList(roleService.getListByPage(pageable));
+			}
+			
+			return ResponseEntity.ok(roleDTO);
+		} catch (Exception exp) {
+			throw new BadRequestException("Yêu câu không hợp lệ. Vui lòng kiểm tra lại!");
 		}
-		
-		return ResponseEntity.ok(roleDTO);
 	}
 }
