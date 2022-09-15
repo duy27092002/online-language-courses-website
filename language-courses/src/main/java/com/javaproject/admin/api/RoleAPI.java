352f8d2bb8a4cbcb5c1961ba7214d0ecea93c07b
CodeProject <nguyenduy27092002@gmail.com>
2022-09-15 15:37:35 +0700

    change ROLE_GIANGVIEN to ROLE_ADMIN

--- language-courses/src/main/java/com/javaproject/admin/api/RoleAPI.java
+++ language-courses/src/main/java/com/javaproject/admin/api/RoleAPI.java
@@ -27,7 +27,7 @@ public class RoleAPI {
 	private SortUtil sortUtil;
 
 	@GetMapping
-	@RolesAllowed("ROLE_GIANGVIEN")
+	@RolesAllowed("ROLE_ADMIN")
 	public ResponseEntity<RoleDTO> showRoleListPage(
 			@Pattern(regexp = "^.+$") @RequestParam(value = "page", required = false, defaultValue = "1") String page,
 			@RequestParam(name = "order-by", required = false, defaultValue = "name") String orderBy,
