f190efe88137ccb7862186c6652d91060ed110f7
CodeProject <nguyenduy27092002@gmail.com>
2022-10-07 21:06:18 +0700

    update view list method

--- language-courses/src/main/java/com/javaproject/admin/api/LanguageAPI.java
+++ language-courses/src/main/java/com/javaproject/admin/api/LanguageAPI.java
@@ -5,8 +5,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,13 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaproject.admin.dto.LanguageDTO;
+import com.javaproject.admin.dto.ResponseDataTableDTO;
 import com.javaproject.admin.exception.BadRequestException;
+import com.javaproject.admin.paging.PagingParam;
 import com.javaproject.admin.service.ILanguageService;
-import com.javaproject.admin.util.SortUtil;
 
 @RestController(value = "languageAPIOfAdmin")
 @RequestMapping(value = "/api/admin/language")
@@ -29,42 +27,10 @@ public class LanguageAPI {
 	@Autowired
 	private ILanguageService languageService;
 
-	@Autowired
-	private SortUtil sortUtil;
-	
-	// test
-//	@Autowired
-//	private ResponseDataTableService responseDTService;
-//	
-//	@GetMapping
-//	public ResponseEntity<?> viewList(@PagingParam(path = "language") ResponseDataTableDTO resDTDTO) {
-//		try {
-//			return ResponseEntity.ok(responseDTService.list(resDTDTO));
-//		} catch (Exception exp) {
-//			throw new BadRequestException("Yêu câu không hợp lệ. Vui lòng kiểm tra lại!");
-//		}
-//	}
-	// end test
-
 	@GetMapping
-	public ResponseEntity<?> viewList(
-			@Pattern(regexp = "^.+$") @RequestParam(value = "page", required = false, defaultValue = "1") String page,
-			@RequestParam(name = "order-by", required = false, defaultValue = "name") String orderBy,
-			@RequestParam(name = "order-type", required = false, defaultValue = "asc") String orderType,
-			@RequestParam(name = "keyword", required = false) String keyword) {
-		LanguageDTO languageDTO = new LanguageDTO();
+	public ResponseEntity<?> viewList(@PagingParam(path = "language") ResponseDataTableDTO resDTDTO) {
 		try {
-			int getPage = Integer.parseInt(page);
-			int pageSize = 2;
-			languageDTO.setCurrentPage(getPage);
-			languageDTO.setPageTotal(languageService.getTotalPage(pageSize));
-			Pageable pageable = PageRequest.of(getPage - 1, pageSize, sortUtil.handleSort(orderBy, orderType));
-			if (keyword != null) {
-				languageDTO.setResultList(languageService.getList(keyword, pageable));
-			} else if (keyword == null) {
-				languageDTO.setResultList(languageService.getList(null, pageable));
-			}
-			return ResponseEntity.ok(languageDTO);
+			return ResponseEntity.ok(languageService.getList(resDTDTO));
 		} catch (Exception exp) {
 			throw new BadRequestException("Yêu câu không hợp lệ. Vui lòng kiểm tra lại!");
 		}
