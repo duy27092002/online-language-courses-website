5e83a646b9c25dc942090ad38d80c70bb35d8569
CodeProject <nguyenduy27092002@gmail.com>
2022-10-11 07:46:50 +0700

    nothing change logic code, remove white space

--- language-courses/src/main/java/com/javaproject/admin/service/IUserService.java
+++ language-courses/src/main/java/com/javaproject/admin/service/IUserService.java
@@ -2,6 +2,6 @@ package com.javaproject.admin.service;
 
 import com.javaproject.admin.dto.UserDTO;
 
-public interface IUserService extends IBaseService<UserDTO> {	
+public interface IUserService extends IBaseService<UserDTO> {
 	UserDTO getUserByEmailOrByPhoneNumber(String email, String phoneNumber);
 }
