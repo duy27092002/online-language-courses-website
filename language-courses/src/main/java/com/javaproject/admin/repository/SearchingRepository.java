27d4567a64abd3e20341c452053891ec24498c67
CodeProject <nguyenduy27092002@gmail.com>
2022-10-12 08:58:05 +0700

    change return type of getSearchList method and add new custom method: getAllList

--- language-courses/src/main/java/com/javaproject/admin/repository/SearchingRepository.java
+++ language-courses/src/main/java/com/javaproject/admin/repository/SearchingRepository.java
@@ -1,12 +1,13 @@
 package com.javaproject.admin.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface SearchingRepository<T, ID> extends JpaRepository<T, ID> {
-	List<T> getSearchList(String keyword, Pageable pageable);
+	Page<T> getSearchList(String keyword, Pageable pageable);
+	
+	Page<T> getAllList(Pageable pageable);
 }
