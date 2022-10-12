0c203061b5f7eb41b56403ff28748c689c76540d
CodeProject <nguyenduy27092002@gmail.com>
2022-10-12 10:27:53 +0700

    remove extends WebMvcConfigurationSupport

--- language-courses/src/main/java/com/javaproject/admin/config/ResolverConfig.java
+++ language-courses/src/main/java/com/javaproject/admin/config/ResolverConfig.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.javaproject.admin.paging.PagingResolver;
 
 @Configuration
-public class ResolverConfig extends WebMvcConfigurationSupport {
+public class ResolverConfig implements WebMvcConfigurer {
 
 	@Override
-	protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new PagingResolver());
 	}
 }
