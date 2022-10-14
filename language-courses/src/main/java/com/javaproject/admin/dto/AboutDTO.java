0a005e58cf34daf1504631d2d43f405b92735d78
CodeProject <nguyenduy27092002@gmail.com>
2022-10-14 23:25:52 +0700

    update validate annotaion

--- language-courses/src/main/java/com/javaproject/admin/dto/AboutDTO.java
+++ language-courses/src/main/java/com/javaproject/admin/dto/AboutDTO.java
@@ -2,7 +2,6 @@ package com.javaproject.admin.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
@@ -17,39 +16,31 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AboutDTO extends BaseDTO<AboutDTO> {
-	@NotBlank(message = "Vui lòng nhập tên website!")
-	@NotNull(message = "Không được để trống tên website!")
-	@Length(min = 1, max = 10, message = "Độ dài tên không được quá 10 ký tự")
+	@Length(min = 1, max = 10, message = "Vui lòng nhập tên website với độ dài không quá 10 ký tự")
 	private String name;
 
-	@NotBlank(message = "Vui lòng nhập địa chỉ!")
-	@NotNull(message = "Không được để trống địa chỉ!")
-	@Length(min = 1, max = 50, message = "Số lượng ký tự đã vượt quá ngưỡng cho phép là 50 ký tự")
+	@Length(min = 1, max = 50, message = "Vui lòng nhập địa chỉ với độ dài không 50 ký tự")
 	private String location;
 
-	@NotNull(message = "Không được để trống email!")
-	@Length(min = 1, max = 50, message = "Email không được vượt quá 50 ký tự")
+	@Length(min = 1, max = 50, message = "Vui lòng nhập email với độ dài không vượt quá 50 ký tự")
 	@Email(message = "Sai định dạng email!")
-	@NotBlank(message = "Vui lòng nhập email!")
 	private String email;
 
-	@NotNull(message = "Không được để trống số điện thoại!")
-	@Pattern(regexp = "(84|0[3|5|7|8|9])+([0-9]{8})\\b", message = "Sai định dạng số điện thoại cá nhân!")
-	@NotBlank(message = "Vui lòng nhập số điện thoại!")
+	@Pattern(regexp = "(84|0[3|5|7|8|9])+([0-9]{8})\\b", message = "Số điện thoại không hợp lệ")
 	private String phoneNumber;
 
-	@NotNull(message = "Không được để trống trường này!")
+	@NotBlank(message = "Không được để trống trường này!")
 	private String twitterLink;
 
-	@NotNull(message = "Không được để trống trường này!")
+	@NotBlank(message = "Không được để trống trường này!")
 	private String facebookLink;
 
-	@NotNull(message = "Không được để trống trường này!")
+	@NotBlank(message = "Không được để trống trường này!")
 	private String instagramLink;
 
-	@NotNull(message = "Không được để trống trường này!")
+	@NotBlank(message = "Không được để trống trường này!")
 	private String youtubeLink;
 
-	@NotNull(message = "Không được để trống trường này!")
+	@NotBlank(message = "Không được để trống trường này!")
 	private String inLink;
 }
