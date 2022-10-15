ce382b29e58f8350b6faee779effa5dbac2d20b0
CodeProject <nguyenduy27092002@gmail.com>
2022-10-15 20:50:06 +0700

    get view title and favicon

--- language-courses/src/main/java/com/javaproject/admin/controller/AdminController.java
+++ language-courses/src/main/java/com/javaproject/admin/controller/AdminController.java
@@ -1,14 +1,21 @@
 package com.javaproject.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.javaproject.util.GetWebsiteDetails;
+
 @Controller(value = "AdminController")
 public class AdminController {
+	@Autowired
+	private GetWebsiteDetails webDetails;
+	
 	@GetMapping(value = {"/quan-tri"})
 	public String dashboardPage(Model model) {
 		model.addAttribute("viewTitle", "Tổng quan");
+		model.addAttribute("favicon", webDetails.getFaviconOrLogo("favicon"));
 		return "/admin/home/index";
 	}
 	
--- language-courses/src/main/java/com/javaproject/admin/controller/ErrorController.java
+++ language-courses/src/main/java/com/javaproject/admin/controller/ErrorController.java
@@ -1,16 +1,23 @@
 package com.javaproject.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javaproject.util.GetWebsiteDetails;
+
 @Controller(value = "ErrorControllerOfAdmin")
 @RequestMapping(value = "/loi")
 public class ErrorController {
+	@Autowired
+	private GetWebsiteDetails webDetails;
+	
 	@GetMapping(value = "/404")
 	public String view404Page(Model model) {
 		model.addAttribute("viewTitle", "Lỗi");
+		model.addAttribute("favicon", webDetails.getFaviconOrLogo("favicon"));
 		return "/common/404";
 	}
 }
