23e74e1ad6259ca247b8f7077756099169a2d347
CodeProject <nguyenduy27092002@gmail.com>
2022-10-17 09:41:58 +0700

    set pre authorize

--- language-courses/src/main/java/com/javaproject/admin/controller/AboutController.java
+++ language-courses/src/main/java/com/javaproject/admin/controller/AboutController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,6 +20,7 @@ import com.javaproject.util.GetWebsiteDetails;
 
 @Controller(value = "AboutControllerOfAdmin")
 @RequestMapping(value = "/quan-tri/thong-tin-website")
+@PreAuthorize("hasAnyRole('ROLE_admin')")
 public class AboutController {
 	@Autowired
 	private IAboutService aboutService;
--- language-courses/src/main/java/com/javaproject/admin/controller/RoleController.java
+++ language-courses/src/main/java/com/javaproject/admin/controller/RoleController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,6 +23,7 @@ import com.javaproject.util.GetWebsiteDetails;
 
 @Controller(value = "RoleControllerOfAdmin")
 @RequestMapping(value = "/quan-tri/vai-tro")
+@PreAuthorize("hasAnyRole('ROLE_admin')")
 public class RoleController {
 	@Autowired
 	private IRoleService roleService;
