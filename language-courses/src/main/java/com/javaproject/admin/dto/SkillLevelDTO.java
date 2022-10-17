47c8fbe1a72ec9b647d9088c945deeba7d415e55
CodeProject <nguyenduy27092002@gmail.com>
2022-10-17 15:22:35 +0700

    remove not null annotation

--- language-courses/src/main/java/com/javaproject/admin/dto/SkillLevelDTO.java
+++ language-courses/src/main/java/com/javaproject/admin/dto/SkillLevelDTO.java
@@ -1,7 +1,6 @@
 package com.javaproject.admin.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -15,7 +14,6 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SkillLevelDTO extends BaseDTO<SkillLevelDTO> {
-	@NotNull(message = "Không được để trống tên kỹ năng!")
 	@NotBlank(message = "Vui lòng nhập tên kỹ năng!")
 	@Length(min = 1, max = 20, message = "Tên kỹ năng không được vượt quá 20 ký tự")
 	private String name;
