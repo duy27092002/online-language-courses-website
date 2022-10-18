0cb88fc832f2fcef488ab0bd7b7ffde14bfbbf1a
CodeProject <nguyenduy27092002@gmail.com>
2022-10-18 22:50:37 +0700

    add return model attribute to contact view

--- language-courses/src/main/java/com/javaproject/web/controller/HomeController.java
+++ language-courses/src/main/java/com/javaproject/web/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.javaproject.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.javaproject.admin.dto.FeedbackDTO;
 
 @Controller(value = "HomeControllerOfWeb")
 public class HomeController {
@@ -36,7 +39,8 @@ public class HomeController {
 	}
 	
 	@GetMapping(value = {"/lien-he"})
-	public String contactPage() {
+	public String contactPage(Model model) {
+		model.addAttribute("feedbackDTO", new FeedbackDTO());
 		return "/web/contact/index";
 	}
 }
