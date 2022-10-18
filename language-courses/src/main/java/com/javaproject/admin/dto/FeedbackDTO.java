1e117c8e497c6730d0e7b2d8e12daaf40e9f987d
CodeProject <nguyenduy27092002@gmail.com>
2022-10-18 22:48:41 +0700

    remove not null annotation

--- language-courses/src/main/java/com/javaproject/admin/dto/FeedbackDTO.java
+++ language-courses/src/main/java/com/javaproject/admin/dto/FeedbackDTO.java
@@ -2,7 +2,6 @@ package com.javaproject.admin.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
@@ -16,18 +15,15 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FeedbackDTO extends BaseDTO<FeedbackDTO> {
-	@NotNull(message = "Không được để trống tên của bạn!")
 	@NotBlank(message = "Vui lòng nhập tên của bạn!")
 	@Length(min = 1, max = 50, message = "Tên không được vượt quá 50 ký tự")
 	private String name;
 
-	@NotNull(message = "Không được để trống email!")
+	@NotBlank(message = "Vui lòng nhập email!")
 	@Length(min = 1, max = 50, message = "Email không được vượt quá 50 ký tự")
 	@Email(message = "Sai định dạng email!")
-	@NotBlank(message = "Vui lòng nhập email!")
 	private String email;
 	
-	@NotNull(message = "Không được để trống câu hỏi!")
 	@NotBlank(message = "Vui lòng nhập câu hỏi của bạn!")
 	@Length(min = 1, max = 1000, message = "Câu hỏi không được vượt quá 1000 ký tự")
 	private String question;
