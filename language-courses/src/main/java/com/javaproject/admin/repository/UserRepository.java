558ecd332363321602e4676a558ad08dd1208e5d
CodeProject <nguyenduy27092002@gmail.com>
2022-10-21 09:39:12 +0700

    update class extends and add two new method to get all list and get search list, comment old method getSearchList

--- language-courses/src/main/java/com/javaproject/admin/repository/UserRepository.java
+++ language-courses/src/main/java/com/javaproject/admin/repository/UserRepository.java
@@ -1,18 +1,21 @@
 package com.javaproject.admin.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.javaproject.admin.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends SearchingRepository<User, Long> {
 	User findByEmail(String email);
 	
 	User findByPhoneNumber(String phoneNumber);
 	
+//	@Query("select u from #{#entityName} u where u.name like %?1% or u.email like %?1%")
+//	List<User> getSearchList(String keyword, Pageable pageable);
 	@Query("select u from #{#entityName} u where u.name like %?1% or u.email like %?1%")
-	List<User> getSearchList(String keyword, Pageable pageable);
+	Page<User> getSearchList(String keyword, Pageable pageable);
+	
+	@Query("select u from #{#entityName} u")
+	Page<User> getAllList(Pageable pageable);
 }
