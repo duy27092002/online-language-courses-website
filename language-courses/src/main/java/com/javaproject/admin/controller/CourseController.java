b90025bf236113a406f4b4d5fd4cee6f615474a2
CodeProject <nguyenduy27092002@gmail.com>
2022-10-21 20:10:46 +0700

    update course controller and list view, but not test yet

--- language-courses/src/main/java/com/javaproject/admin/controller/CourseController.java
+++ language-courses/src/main/java/com/javaproject/admin/controller/CourseController.java
@@ -1,5 +1,170 @@
 package com.javaproject.admin.controller;
 
-public class CourseController {
+import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.javaproject.admin.dto.CourseDTO;
+import com.javaproject.admin.dto.ResponseDataTableDTO;
+import com.javaproject.admin.paging.PagingParam;
+import com.javaproject.admin.service.ICourseService;
+import com.javaproject.util.GetWebsiteDetails;
+
+@Controller(value = "CourseControllerOfAdmin")
+@RequestMapping(value = "/quan-tri/khoa-hoc")
+public class CourseController {
+	@Autowired
+	private ICourseService courseService;
+
+	@Autowired
+	private GetWebsiteDetails webDetails;
+
+	private void setViewTitleOrFaviconAttribute(String viewTitle, Model model) {
+		model.addAttribute("viewTitle", viewTitle);
+		model.addAttribute("favicon", webDetails.getFaviconOrLogo("favicon"));
+	}
+
+	@GetMapping(value = "/danh-sach")
+	public String viewListPage(@PagingParam(path = "khoa-hoc") ResponseDataTableDTO resDTDTO,
+			RedirectAttributes redirectModel, Model model) {
+		setViewTitleOrFaviconAttribute("Danh sách khóa học", model);
+		try {
+			ResponseDataTableDTO resultList = courseService.getList(resDTDTO);
+			model.addAttribute("resultList", resultList);
+
+			String getOrderType = resultList.getOrderType().equalsIgnoreCase("asc") ? "desc" : "asc";
+			model.addAttribute("orderType", getOrderType);
+
+			if (resDTDTO.getKeyword() != null) {
+				model.addAttribute("keyword", resDTDTO.getKeyword());
+			}
+		} catch (Exception e) {
+			return viewErrorPage(redirectModel);
+		}
+		return "/admin/course/list";
+	}
+
+	@GetMapping(value = "/them-moi")
+	public String viewCreatePage(Model model) {
+		setViewTitleOrFaviconAttribute("Thêm mới khóa học", model);
+		model.addAttribute("courseDTO", new CourseDTO());
+		return "/admin/course/create-or-edit";
+	}
+
+	@PostMapping(value = "/them-moi")
+	public String create(@Valid @ModelAttribute("courseDTO") CourseDTO courseDTO, BindingResult bindingResult,
+			RedirectAttributes redirectModel, Model model) {
+		return save("create", courseDTO, bindingResult, redirectModel, model);
+	}
+
+	@GetMapping(value = "/chi-tiet")
+	public String viewDetailsPage(@Pattern(regexp = "^.+$") @RequestParam(value = "id") String id,
+			RedirectAttributes redirectModel, Model model) {
+		return redirectPage(id, "details", redirectModel, model);
+	}
+
+	@GetMapping(value = "/chinh-sua")
+	public String viewUpdatePage(@Pattern(regexp = "^.+$") @RequestParam(value = "id") String id,
+			RedirectAttributes redirectModel, Model model) {
+		return redirectPage(id, "create-or-edit", redirectModel, model);
+	}
+
+	@PostMapping(value = "/chinh-sua")
+	public String update(@Valid @ModelAttribute("courseDTO") CourseDTO courseDTO, BindingResult bindingResult,
+			RedirectAttributes redirectModel, Model model) {
+		return save("update", courseDTO, bindingResult, redirectModel, model);
+	}
+
+	private String redirectPage(String id, String action, RedirectAttributes redirectModel, Model model) {
+		if (action.equalsIgnoreCase("details")) {
+			setViewTitleOrFaviconAttribute("Chi tiết khóa học", model);
+		} else {
+			setViewTitleOrFaviconAttribute("Chỉnh sửa khóa học", model);
+			model.addAttribute("courseDTO", new CourseDTO());
+		}
+
+		try {
+			Long getCourseId = Long.parseLong(id);
+			model.addAttribute("courseDetails", courseService.getDetails(getCourseId).get(0));
+		} catch (Exception e) {
+			return viewErrorPage(redirectModel);
+		}
+		return "/admin/course/" + action;
+	}
+
+	// hàm chung cho thao tác thêm mới và cập nhật
+	private String save(String formAction, @Valid @ModelAttribute("courseDTO") CourseDTO courseDTO,
+			BindingResult bindingResult, RedirectAttributes redirectModel, Model model) {
+		CourseDTO getOldCourseById = null;
+		if (formAction.equalsIgnoreCase("create")) {
+			setViewTitleOrFaviconAttribute("Thêm mới khóa học", model);
+		} else {
+			setViewTitleOrFaviconAttribute("Chỉnh sửa khóa học", model);
+			getOldCourseById = courseService.getDetails(courseDTO.getId()).get(0);
+		}
+
+		if (bindingResult.hasErrors()) {
+			if (formAction.equalsIgnoreCase("update")) {
+				model.addAttribute("courseDetails", courseDTO);
+			}
+			return "/admin/course/create-or-edit";
+		}
+
+		String successMess = null;
+		String errorMess = null;
+		try {
+			CourseDTO getCourseByName = courseService.getCourseByName(courseDTO.getName());
+
+			if (formAction.equalsIgnoreCase("create")) {
+				if (getCourseByName != null) {
+					return isExitName(model);
+				}
+				successMess = "Thêm mới thành công";
+				errorMess = "Thêm mới thất bại";
+			} else {
+				if (!getOldCourseById.getName().equalsIgnoreCase(courseDTO.getName())) {
+					if (getCourseByName != null) {
+						model.addAttribute("courseDetails", courseDTO);
+						return isExitName(model);
+					}
+				}
+				successMess = "Chỉnh sửa thành công";
+				errorMess = "Chỉnh sửa thất bại";
+			}
+
+			CourseDTO getLanguageAfterSave = courseService.save(courseDTO);
+			if (getLanguageAfterSave != null) {
+				redirectModel.addFlashAttribute("typeAlert", "success");
+				redirectModel.addFlashAttribute("mess", successMess);
+			}
+		} catch (Exception ex) {
+			redirectModel.addFlashAttribute("typeAlert", "danger");
+			redirectModel.addFlashAttribute("mess", errorMess);
+		}
+
+		return "redirect:/quan-tri/khoa-hoc/danh-sach";
+	}
+
+	// hiển thị lỗi trùng tên khóa học
+	private String isExitName(Model model) {
+		model.addAttribute("isExitName", "Tên khóa học này đã tồn tại");
+		return "/admin/course/create-or-edit";
+	}
+
+	// hiển thị trang lỗi khi không tìm thấy dữ liệu
+	private String viewErrorPage(RedirectAttributes redirectModel) {
+		redirectModel.addFlashAttribute("returnPage", "tổng quan");
+		redirectModel.addFlashAttribute("returnPageUrl", "/quan-tri");
+		return "redirect:/loi/404";
+	}
 }
--- language-courses/src/main/webapp/WEB-INF/views/admin/course/list.jsp
+++ language-courses/src/main/webapp/WEB-INF/views/admin/course/list.jsp
@@ -1,12 +1,114 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+	pageEncoding="UTF-8"%>
+<%@include file="/WEB-INF/views/common/admin/sidebar.jsp"%>
+<!-- Main Content -->
+<div id="content">
+	<%@include file="/WEB-INF/views/common/admin/topbar.jsp"%>
 
-</body>
-</html>
+	<div class="container-fluid">
+		<h1 class="text-center">Danh sách khóa học</h1>
+		<c:if test="${mess != null}">
+			<div class="alert alert-${typeAlert} alert-dismissible fade show"
+				role="alert">
+				<strong>${mess}</strong>
+				<button type="button" class="close" data-dismiss="alert"
+					aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		</c:if>
+		<div class="row">
+			<div class="col-12 col-sm-12 col-md-6 col-lg-6">
+				<a class="btn btn-sm btn-primary mb-4"
+					href="/quan-tri/nguoi-dung/them-moi"><i
+					class="fas fa-plus mr-2"></i>Thêm mới</a>
+			</div>
+			<div class="col-12 col-sm-12 col-md-6 col-lg-6">
+				<form action="/quan-tri/nguoi-dung/danh-sach" method="get"
+					class="float-sm-left float-md-right float-lg-right form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+					<div class="input-group">
+						<input name="keyword" type="text"
+							class="form-control bg-light small" placeholder="Nhập từ khóa"
+							value="${keyword}">
+						<div class="input-group-append">
+							<button class="btn btn-primary" type="submit">
+								<i class="fas fa-search fa-sm"></i>
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="card o-hidden border-0 shadow-lg mb-4 mt-3">
+			<div class="card-body p-5">
+				<div class="table-responsive">
+					<table class="table table-striped text-center">
+						<thead>
+							<tr>
+								<c:if test="${resultList.keyword == null}">
+									<c:set var="pagingParam"
+										value="orderBy=name&orderType=${orderType}"></c:set>
+								</c:if>
+								<c:if test="${resultList.keyword != null}">
+									<c:set var="pagingParam"
+										value="keyword=${resultList.keyword}&orderBy=name&orderType=${orderType}"></c:set>
+								</c:if>
+								<th scope="col">Ảnh đại diện</th>
+								<th scope="col"><a
+									href="?page=${resultList.page}&${pagingParam}"
+									class="text-body" style="text-decoration: none;">Tên khóa
+										học <i class="fas fa-arrows-alt-v ml-1"></i>
+								</a></th>
+								<th scope="col">Ngôn ngữ</th>
+								<th scope="col">Giá</th>
+								<th scope="col">Thời gian phát hành</th>
+								<th scope="col">Trạng thái</th>
+								<th scope="col">Thao tác</th>
+							</tr>
+						</thead>
+						<tbody>
+							<c:forEach items="${resultList.data}" var="course">
+								<tr>
+									<td><img class="border img-fluid" style="width: 70px;"
+										src="${course.thumbnail}" alt="thumbnail" /></td>
+
+									<td>${course.name}</td>
+									<td>Ngôn ngữ</td>
+									<td>${course.price}</td>
+									<td><c:if test="${course.releaseTime == null}">
+											<c:out value="Chưa có" />
+										</c:if></td>
+									<td><c:if test="${course.status == 1 }">
+											<c:set var="textColor" value="warning"></c:set>
+											<c:set var="tooltipTitle" value="Đang kiểm duyệt"></c:set>
+											<c:set var="toggleStatus" value="off"></c:set>
+										</c:if> <c:if test="${course.status == 0 }">
+											<c:set var="textColor" value="danger"></c:set>
+											<c:set var="tooltipTitle" value="Đã hủy"></c:set>
+											<c:set var="toggleStatus" value="off"></c:set>
+										</c:if> <c:if test="${course.status == 2 }">
+											<c:set var="textColor" value="success"></c:set>
+											<c:set var="tooltipTitle" value="Đang hoạt động"></c:set>
+											<c:set var="toggleStatus" value="on"></c:set>
+										</c:if> <i class="fas fa-toggle-${toggleStatus} text-${textColor}"
+										data-toggle="tooltip" title="${tooltipTitle}"></i></td>
+									<td><button class="btn btn-sm border-primary">
+											<a href="/quan-tri/khoa-hoc/chi-tiet?id=${course.id}"><i
+												class="fas fa-eye" data-toggle="tooltip"
+												title="Xem chi tiết"></i></a> | <a
+												href="/quan-tri/khoa-hoc/chinh-sua?id=${course.id}"><i
+												class="fas fa-edit" data-toggle="tooltip" title="Chỉnh sửa"></i></a>
+										</button></td>
+								</tr>
+							</c:forEach>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<%@include file="/WEB-INF/views/common/pagination.jsp"%>
+</div>
+<!-- End of Main Content -->
+<%@include file="/WEB-INF/views/common/admin/footer.jsp"%>
