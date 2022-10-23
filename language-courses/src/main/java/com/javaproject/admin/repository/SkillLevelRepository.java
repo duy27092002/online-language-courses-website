2f562b1b36f0e0284291300108ddbe99ed4d4800
CodeProject <nguyenduy27092002@gmail.com>
2022-10-23 20:28:17 +0700

    add new method to get skill level list by status

--- language-courses/src/main/java/com/javaproject/admin/repository/SkillLevelRepository.java
+++ language-courses/src/main/java/com/javaproject/admin/repository/SkillLevelRepository.java
@@ -1,5 +1,7 @@
 package com.javaproject.admin.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +18,6 @@ public interface SkillLevelRepository extends SearchingRepository<SkillLevel, Lo
 	
 	@Query("select skl from #{#entityName} skl")
 	Page<SkillLevel> getAllList(Pageable pageable);
+	
+	List<SkillLevel> findByStatus(int status);
 }
