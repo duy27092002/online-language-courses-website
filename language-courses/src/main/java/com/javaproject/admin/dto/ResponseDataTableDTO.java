d91810f4532c9ec5095b0993202eae91a8571574
CodeProject <nguyenduy27092002@gmail.com>
2022-10-25 15:26:15 +0700

    add new method to get video list by course id

--- language-courses/src/main/java/com/javaproject/admin/dto/ResponseDataTableDTO.java
+++ language-courses/src/main/java/com/javaproject/admin/dto/ResponseDataTableDTO.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import com.javaproject.admin.entity.Video;
 import com.javaproject.admin.repository.SearchingRepository;
+import com.javaproject.admin.repository.VideoRepository;
 import com.javaproject.admin.util.SortUtil;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +21,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDataTableDTO {
+	private Long id;
+
 	private String path;
 
 	private Integer page;
@@ -37,9 +41,10 @@ public class ResponseDataTableDTO {
 
 	private List<?> data;
 
-	public ResponseDataTableDTO(String path, Integer page, Integer pageSize, String keyword, String orderBy,
+	public ResponseDataTableDTO(String path, Long id, Integer page, Integer pageSize, String keyword, String orderBy,
 			String orderType) {
 		this.path = path;
+		this.id = id;
 		this.page = page;
 		this.pageSize = pageSize;
 		this.keyword = keyword;
@@ -53,22 +58,44 @@ public class ResponseDataTableDTO {
 		SortUtil sortUtil = new SortUtil();
 		Pageable pageable = PageRequest.of(page - 1, pageSize, sortUtil.handleSort(orderBy, orderType));
 		List<T> resultList = new ArrayList<>();
-		//List<?> getListByPage = null;
 		Page<?> getListByPage = null;
 		long total = 0;
 		if (keyword == null) {
-//			getListByPage = repository.findAll(pageable).getContent();
-//			total = repository.count();
 			getListByPage = repository.getAllList(pageable);
 		} else {
 			getListByPage = repository.getSearchList(keyword, pageable);
-//			total = repository.getSearchList(keyword, null).size() ;
 		}
-		// test
 		total = getListByPage.getTotalElements();
 		for (Object object : getListByPage) {
 			T dto = dtoObj.newInstance();
 			BeanUtils.copyProperties(object, dto);
+			resultList.add(dto);
+		}
+		this.setData(resultList);
+		this.setTotalOfItem((int) total);
+		this.setTotalOfPage((int) Math.ceil((double) total / pageSize));
+		return this;
+	}
+
+	public ResponseDataTableDTO getList(VideoRepository repository, Long id, String keyword) throws Exception {
+		SortUtil sortUtil = new SortUtil();
+		Pageable pageable = PageRequest.of(page - 1, pageSize, sortUtil.handleSort(orderBy, orderType));
+		List<VideoDTO> resultList = new ArrayList<>();
+		Page<Video> getListByPage = null;
+		long total = 0;
+		if (keyword == null) {
+			try {
+				getListByPage = repository.getAllList(id, pageable);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			getListByPage = repository.getSearchList(id, keyword, pageable);
+		}
+		total = getListByPage.getTotalElements();
+		for (Video videoEntity : getListByPage) {
+			VideoDTO dto = new VideoDTO();
+			BeanUtils.copyProperties(videoEntity, dto);
 			resultList.add(dto);
 		}
 		this.setData(resultList);
