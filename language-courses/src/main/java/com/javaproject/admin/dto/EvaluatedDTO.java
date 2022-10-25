3c71d482035318a40d278193aa77696be967b6eb
CodeProject <nguyenduy27092002@gmail.com>
2022-10-25 20:35:25 +0700

    update response properties

--- language-courses/src/main/java/com/javaproject/admin/dto/EvaluatedDTO.java
+++ language-courses/src/main/java/com/javaproject/admin/dto/EvaluatedDTO.java
@@ -1,9 +1,11 @@
 package com.javaproject.admin.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
+
+import com.javaproject.admin.entity.Course;
+import com.javaproject.admin.entity.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,16 +21,14 @@ public class EvaluatedDTO extends BaseDTO<EvaluatedDTO> {
 	
 	private Long userId;
 	
-	@NotNull(message = "Không được để trống nội dung đánh giá!")
 	@NotBlank(message = "Vui lòng nhập nội dung đánh giá!")
 	@Length(min = 1, max = 1000, message = "Nội dung đánh giá không được vượt quá 1000 ký tự")
 	private String content;
 	
-	@NotNull(message = "Không được để trống điểm đánh giá!")
 	private int point;
 	
 	// response
-	private String courseName;
+	private Course course;
 	
-	private String emailOfStudent;
+	private User user;
 }
