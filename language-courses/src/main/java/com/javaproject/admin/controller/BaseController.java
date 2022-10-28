eafbf19d070ddcbd9abc19bf25821caafb805607
CodeProject <nguyenduy27092002@gmail.com>
2022-10-28 23:08:49 +0700

    create base controller to set common funtion or default data: view title, favicon, view error page, redirect notification

---
+++ language-courses/src/main/java/com/javaproject/admin/controller/BaseController.java
@@ -0,0 +1,34 @@
+package com.javaproject.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.javaproject.util.GetWebsiteDetails;
+
+@Controller(value = "BaseControllerOfAdmin")
+public class BaseController {
+	@Autowired
+	private GetWebsiteDetails webDetails;
+
+	// load tên trang + favicon
+	protected void setViewTitleOrFaviconAttribute(String viewTitle, Model model) {
+		model.addAttribute("viewTitle", viewTitle);
+		model.addAttribute("favicon", webDetails.getFaviconOrLogo("favicon"));
+	}
+
+	// hiển thị trang lỗi
+	protected String viewErrorPage(RedirectAttributes redirectModel) {
+		redirectModel.addFlashAttribute("returnPage", "tổng quan");
+		redirectModel.addFlashAttribute("returnPageUrl", "/quan-tri");
+		return "redirect:/loi/404";
+	}
+
+	// thông báo trạng thái của quá trình thao tác (thành công hoặc thất bại khi
+	// thêm mới hoặc chỉnh sửa)
+	protected void redirectNotification(RedirectAttributes redirectModel, String mess, String typeAlert) {
+		redirectModel.addFlashAttribute("typeAlert", typeAlert);
+		redirectModel.addFlashAttribute("mess", mess);
+	}
+}
